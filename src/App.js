8ebc5211050afc869d61e067c7d761de21a54557
chisato410 <it242243@trident.ac.jp>
2025-08-01 14:38:32 +0900

    ルーティング設定

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import TypeQuiz from "./pages/TypeQuiz";
+import Bingo from "./pages/Bingo";
+import Shiritori from "./pages/Shiritori";
+import GenerationQuiz from "./pages/GenerationQuiz";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/type-quiz" element={<TypeQuiz />} />
+        <Route path="/bingo" element={<Bingo />} />
+        <Route path="/shiritori" element={<Shiritori />} />
+        <Route path="/generation-quiz" element={<GenerationQuiz />} />
+      </Routes>
+    </Router>
   );
 }
 
